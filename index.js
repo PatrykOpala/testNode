876ffe536d926b50dec5082ff2525ef091b34355
PatrykOpala <77116693+PatrykOpala@users.noreply.github.com>
2022-02-08 17:26:48 +0100

    0.0.35

--- index.js
+++ index.js
@@ -9,17 +9,17 @@ function createWindow() {
     width: 850,
     height: 630,
     icon: './img/logo.ico',
+    backgroundColor: '#ffffff',
     webPreferences: {
-      // nodeIntegration: true,
-      // contextIsolation: false,
-      // enableRemoteModule: true,
-      preload: path.join(__dirname, 'DocumentOfficePreload.js')
-    }
+      preload: path.join(__dirname, 'DocumentOfficePreload.js'),
+      imageAnimationPolicy: 'noAnimation',
+      v8CacheOptions: 'none'
+    },
+    show: false
+    // frame: false
   })
   win.loadFile('index.html')
-  // win.maximize()
-  // win.webContents.openDevTools()
-  // win.webContents.clearHistory()
+  win.once('ready-to-show', () => { win.show() })
 }
 
 app.whenReady().then(() => {
