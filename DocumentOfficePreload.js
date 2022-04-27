bb4edb2ac177f5793dfac6b3d0093a8cf05261ad
Patryk Opala <patrykopala265@gamil.com>
2022-04-27 19:16:04 +0200

    0.0.62

--- DocumentOfficePreload.js
+++ DocumentOfficePreload.js
@@ -1,8 +1,14 @@
 const { contextBridge } = require('electron');
 const decompress = require('decompress');
 const fs = require('fs');
+const { Paragraph, TextRun, Document, Packer, Table } = require("docx");
 
 contextBridge.exposeInMainWorld('mFile', {
     de: decompress,
-    fileSystem: fs
+    fileSystem: fs,
+    paragraph: Paragraph,
+    textRun: TextRun,
+    document: Document,
+    packer: Packer,
+    table: Table
 })
